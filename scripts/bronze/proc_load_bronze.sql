a7f2c422529cd3f49adf79e96b74b8252a9484f5
Prathmesh Bakkar <96971347+Prathmesh-Bakkar@users.noreply.github.com>
2026-03-25 20:40:43 +0530

    Change procedure definition to CREATE OR ALTER

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -23,7 +23,7 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
-ALTER   PROCEDURE [bronze].[load_bronze] AS
+CREATE OR ALTER PROCEDURE [bronze].[load_bronze] AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
 	BEGIN TRY
